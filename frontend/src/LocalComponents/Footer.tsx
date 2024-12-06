b48e8b36985b662df9321855d4dd90c5d277a0ee
smakon <25469@inbox.ru>
2024-12-07 00:26:54 +0300

    [A]: новый класс Characters

--- frontend/src/LocalComponents/Footer.tsx
+++ frontend/src/LocalComponents/Footer.tsx
@@ -1,17 +1,12 @@
 import '../Scss/Footer/Footer.css'
-import { useState } from 'react';
 import { FooterM } from './Footers/FooterM'
 import { FooterD } from './Footers/FooterD'
-
 
 interface FooterProps{
    currentDevice: string;
 }
 
-
 export const Footer = ({currentDevice}: FooterProps) => {
-   
-
    if (currentDevice == 'desktop') { return <FooterD/> }
    else { return <FooterM/> }
 
